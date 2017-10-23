19cb90eb175cd6b311576d3cb70818e95c45d569
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-10-22 23:04:03 -0500

    Created 2 classes FindDistance and Point

---
+++ sandbox/src/main/java/ru/stqa/school/sandbox/FindDistance.java
@@ -0,0 +1,12 @@
+package ru.stqa.school.sandbox;
+
+public class FindDistance {
+
+  public static void main(String[] args) {
+
+    Point p = new Point (4, 2);
+
+    System.out.println(" Distance between 2 points " + " = " + p.distance());
+
+  }
+}
