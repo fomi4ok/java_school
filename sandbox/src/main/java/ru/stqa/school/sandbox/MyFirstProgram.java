2569857931763c799a1adddb72384356f5d921ad
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-10-22 19:44:16 -0500

    adding functions/methods

--- sandbox/src/main/java/ru/stqa/school/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/school/sandbox/MyFirstProgram.java
@@ -1,7 +1,34 @@
 package ru.stqa.school.sandbox;
 
 public class MyFirstProgram {
- public static void main(String[] args) {
-System.out.println("Hello, world!");
-}
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Marina");
+
+    double l = 5;
+    System.out.println( "Площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 6;
+    double b = 4;
+    System.out.println( "Площадь прямоугольника со сторонами " + a  + " и " + b + " = " + area(a, b));
+
+
+  }
+
+  public static void hello (String somebody) {
+
+    System.out.println("Hello, " + somebody +"!");
+
+  }
+  public static double area (double l) {
+
+    return l * l;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+
+
+  }
+
 }
