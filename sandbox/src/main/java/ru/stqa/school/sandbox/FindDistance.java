8281e88f3b5cb8dab760b0b97a3bc9683fb497d7
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-10-24 12:28:12 -0500

    created tests in PointTests class

--- sandbox/src/main/java/ru/stqa/school/sandbox/FindDistance.java
+++ sandbox/src/main/java/ru/stqa/school/sandbox/FindDistance.java
@@ -4,8 +4,10 @@ public class FindDistance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point (4.0, 2.0);
-    Point p2 = new Point (6.0,8.0);
+    Point p1 = new Point(5.0, 2.0);
+    Point p2 = new Point(10.0, 8.0);
+
+
 
     System.out.println(" Distance between 2 points " + " = " + p1.distance(p2));
 
---
+++ sandbox/src/test/java/ru/stqa/school/sandbox/PointTests.java
@@ -0,0 +1,38 @@
+package ru.stqa.school.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance() {
+
+    Point p1 = new Point(1.0, 3.0);
+    Point p2 = new Point(4.0, 7.0);
+    Assert.assertEquals(p1.distance(p2), 5.0);
+
+  }
+
+  @Test
+
+  public void testDistanceOfThePointToItself() {
+
+    Point p1 = new Point(2.0, 3.0);
+    Assert.assertEquals(p1.distance(p1), 0.0);
+
+  }
+
+  @Test
+
+  public void testWithNegativeAttributes() {
+
+    Point p1 = new Point(-2.0, -3.0);
+    Point p2 = new Point(1.0, 1.0);
+    Assert.assertEquals(p1.distance(p2), 5.0);
+
+
+  }
+
+
+}
