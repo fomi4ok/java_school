cb16008f258f5f806ffceda729620618cbb4e846
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-11-13 23:39:14 -0600

    selecting element from the list by index

--- addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/appmanager/GroupHelper.java
@@ -34,7 +34,9 @@ public class GroupHelper extends HelperBase{
     click(By.xpath("//div[@id='content']/form/input[5]"));
   }
 
-  public void selectGroup() { click(By.name("selected[]"));
+  public void selectGroup(int index) {
+    wd.findElements(By.name("selected[]")).get(index).click();
+
   }
   //div[@id='content']/form/span[2]/input"
 
