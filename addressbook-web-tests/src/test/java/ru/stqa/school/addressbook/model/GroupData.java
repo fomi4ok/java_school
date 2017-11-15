12863f3df82d2b369b44cadf81674ac889acf612
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-11-14 23:20:35 -0600

    sort using anonymous functions

--- addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/model/GroupData.java
@@ -23,11 +23,27 @@ public class GroupData {
     this.footer = footer;
   }
 
-  public GroupData( String name, String header, String footer) {
-    this.id = 0;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
+
   }
 
   public String getName() {
@@ -50,21 +66,4 @@ public class GroupData {
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/test/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/test/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.school.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -12,26 +13,22 @@ import java.util.List;
 public class GroupCreationTest extends TestBase {
 
   @Test
-    public void testGroupCreation() {
+  public void testGroupCreation() {
 
-        app.getNavigationHelper().goToGroupPage();
-        List<GroupData>  before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
-        app.getGroupHelper().createGroup(group);
-        List<GroupData>  after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals( after.size(), before.size() + 1);
+    app.getNavigationHelper().goToGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
-        for (GroupData g : after) {
-          if (g.getId() > max) {
-            max = g.getId();
-          }
-        }
-        group.setId(max);
-        before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals( before, after);
 
-    }
+  }
 
 }
