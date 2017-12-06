6fed24f4fa06b00c9fe4e596ab26486755d7804f
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2017-12-05 22:19:52 -0600

    adding logging (logback)

--- addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/school/addressbook/test/TestBase.java
@@ -1,12 +1,21 @@
 package ru.stqa.school.addressbook.test;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.school.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 
   protected static final ApplicationManager app
@@ -19,9 +28,27 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite (alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
+
+
+ @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+   logger.info("start test" + m.getName() + "with parameters" + Arrays.asList(p));
+
+
+
+ }
+
+ @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m) {
+   logger.info("stop test" + m.getName());
+
+
+
+ }
+
 
 }
